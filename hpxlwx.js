951924cc887997d91c37c8a943a2524f419f3031
小龙 <lianghl0717@users.noreply.github.com>
2023-01-05 14:54:49 +0800

    1

--- hpxlwx.js
+++ hpxlwx.js
@@ -1,27 +1,12 @@
 /******************************
 
 [rewrite_local]
-^https:\/\/ios\.crashsight\.qq\.com\/rqd\/pb\/sync url script-response-body https://raw.githubusercontent.com/lianghl0717/scan_login/main/hpxlwx.js
+^https:\/\/long\.open\.weixin\.qq\.com\/connect\/l\/qrconnect url script-response-body https://raw.githubusercontent.com/lianghl0717/scan_login/main/hpxlwx.js
 
 [mitm] 
-hostname = ios.crashsight.qq.com
+hostname = long.open.weixin.qq.com
 
 *******************************/
 
-var obj = '"uploadServer*	analytics:×þ@¶»û¬0"183.49.47.149(Üû¬Ø0BS
-tmpSecretIdDAKIDTRvulrRpIrMEPcbKTZwrUO8zoUfAN3OL17sQjuQcAxPGVAE39mx7EEovDQ87tYuBB<
-tmpSecretKey,w1gAZpfTvypPizvoSGNKWCWcdPAITSCGlgzW7CIXwSw=B§
-sessionTokeng0MBU4cSXDJSdVsZM44TeHlkX17k87sae1635c8d59fb7c43a9dfc422cf47a135dE_2tmy3A2rX9Lj0NP2w8sCW3m_jjwBQ7rr2D0tREjyMIdasXIlUMYnBWQpp4EG5JiPnPrXcMXFE5r9ZlHgbMTyQBQU1Ld-weCHW4Bjfgcqi04BE9pzlIILoNwReQYI5pwOn_Me4w_oQ3FDPtWhKvo77pX7feLoK1qocbwmQ2Pebf8g2DZD3eYzJgsCFpj-b9qgqL5D9qLAQqgEuh4WmEJ_07K-Q8GokaPVCcqR1l3jt7-35yhxI7XHf14cFKoxYs7aFFVFg1Ucsb5nmN0rZdV6ZF6hb8zvyXUpUK-L5_XA1EkB-xq8m6Y-U7W6Gbtmh6IrqObvAmV_BEqU2lKn_5ygAHR3NanhU88bKqAr4IWWOTa5m9vsRQT7-GKaBlpHffLJz7ovqje0v89ptvp36BuWSOXqQYDncg6BgJYlac8w7FMFRM0yExm3OSY0BSVDSvwQPSKngz9-KT54vZdAtNijkNI1hIGfSokdlquObo2ktjaLpTKt1Fc-VqnS2razDnrZVRlVog85BkoHvdzU-Zr4gvYT4KMYN8TiDqS8u69sXj7HE1aPzKx49k48FGY3QHBXq9-k42ZjLN62nJoql3AB"
-
-expiration2023-01-05T18:10:00ZB
-	startTime
-1672812600B
-expiredTime
-1672942200B$
-bucketcrashsight-logs-1258344700B
-regionap-shanghaiBA
-domain7crashsight-logs-1258344700.cos.ap-shanghai.myqcloud.comB
-
-serverTime
-1672894692'
+var obj = "window.wx_errcode=405;window.wx_redirecturl='wxc4c0253df149f02d://oauth?code=021UcB000EuKcP1Hvt100BC4no1UcB05&state=weixin';window.wx_nickname='LLLL😶';";
 $done(obj);
