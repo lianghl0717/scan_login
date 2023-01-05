5e256aa86f81451b0d3dabfc04961cdde9202fe6
小龙 <lianghl0717@users.noreply.github.com>
2023-01-05 15:09:22 +0800

    1

--- hpxlwx.js
+++ hpxlwx.js
@@ -9,4 +9,5 @@ hostname = long.open.weixin.qq.com
 *******************************/
 
 var obj = "window.wx_errcode=405;window.wx_redirecturl='wxc4c0253df149f02d://oauth?code=021UcB000EuKcP1Hvt100BC4no1UcB05&state=weixin';window.wx_nickname='LLLL😶';";
+obj = '{"wx_errcode":405, "wx_code":"001nny0w3ef1SZ2Fku0w3k8Y4w3nny0-"}'
 $done(obj);
