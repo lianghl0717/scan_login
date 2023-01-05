c857611a4b2587cf2e97dfa603cc2e8ff57354b4
小龙 <lianghl0717@users.noreply.github.com>
2023-01-05 11:43:57 +0800

    1

--- hpxl.js
+++ hpxl.js
@@ -6,4 +6,4 @@
 
 hostname = openmobile.qq.com
 
-$done(`_Callback({"ret":0,"url":"auth://www.qq.com?#access_token=C5EB0F50AC7338D29A8425DA86E90755&expires_in=7776000&openid=2C66675250AE0FC00632AE7F734715CE&pay_token=683549F9AA55D03883BA63F8E86EDA23&state=test&ret=0&pf=openmobile_ios&pfkey=a9ca28439f71c373ded4090d24455465&auth_time=1672845708&page_type=0"});`);
+$done('_Callback({"ret":0,"url":"auth://www.qq.com?#access_token=C5EB0F50AC7338D29A8425DA86E90755&expires_in=7776000&openid=2C66675250AE0FC00632AE7F734715CE&pay_token=683549F9AA55D03883BA63F8E86EDA23&state=test&ret=0&pf=openmobile_ios&pfkey=a9ca28439f71c373ded4090d24455465&auth_time=1672845708&page_type=0"});');
